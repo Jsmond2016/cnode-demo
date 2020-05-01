dc20311e2d14327d42cdc0b74194a7f15caa852f
黄锦 <jin.huang02@hand-china.com>
2020-05-01 14:31:18 +0800

    feat(cnode-demo): 完成 header 组件和 PostList 组件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Axios from 'axios'
+import { formatDate, tabFormatter } from './utis/filters'
 
+import './assets/reset.css'
+
+Vue.prototype.$http = Axios
 Vue.config.productionTip = false
+
+Vue.filter('formatDate', formatDate)
+Vue.filter('tabFormatter', tabFormatter)
 
 /* eslint-disable no-new */
 new Vue({
